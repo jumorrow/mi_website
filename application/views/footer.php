6595108217ba1d5a434002aefd378bb66ec2f7ea
Justin Morrow <jumorrow@magento.com>
2018-06-08 11:07:14 -0500

    updated footer info

--- application/views/footer.php
+++ application/views/footer.php
@@ -6,7 +6,9 @@
             <ul class="copyright">
                 <li>&copy; Copyright <?php echo get_current_year();?> MorrowInteractive.com</li>
             </ul>
+            <p class="copyright">Website Built with <a target="_blank" href="https://www.codeigniter.com/">CodeIgniter</a></p>
         </div>
+        
         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
     </div>
 </footer>
