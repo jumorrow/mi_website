cdc13c44b3251f4d1e2710554c653d0ccd884355
Justin Morrow <jumorrow@MacBook-Pro-6.local>
2020-02-29 12:57:29 -0600

    adding recaptchav3

--- application/views/final_scripts.php
+++ application/views/final_scripts.php
@@ -3,7 +3,13 @@
 <script src="<?php echo get_jquery_assets();?>" type="text/javascript"></script>
 
 <script type="text/javascript">
-window.jQuery || document.write('<script src="<?php echo get_asset_path('js','jquery-1.10.2.min.js');?>"><\/script>')
+window.jQuery || document.write('<script src="<?php echo get_asset_path('js','jquery-1.10.2.min.js');?>"><\/script>');
+grecaptcha.ready(function() {
+    grecaptcha.execute('6LdJgd0UAAAAACu4JhWz8JJHgWUbgbIFlWOx1AUV', {action: 'homepage'}).then(function(token) {
+       ...
+    });
+});
+
 </script>
 
 <script type="text/javascript" src="<?php echo get_asset_path('js','jquery-migrate-1.2.1.min.js');?>"></script>
@@ -11,4 +17,5 @@ window.jQuery || document.write('<script src="<?php echo get_asset_path('js','jq
 <script src="<?php echo get_asset_path('js','waypoints.js');?>" type="text/javascript"></script>
 <script src="<?php echo get_asset_path('js','jquery.fittext.js');?>" type="text/javascript"></script>
 <script src="<?php echo get_asset_path('js','magnific-popup.js');?>" type="text/javascript"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6LdJgd0UAAAAACu4JhWz8JJHgWUbgbIFlWOx1AUV"></script>
 <script src="<?php echo get_asset_path('js','init.js');?>" type="text/javascript"></script>
